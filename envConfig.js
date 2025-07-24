b192692179a5cfa13f035cd51658bb0b808de184
Nakkul gulati <74372812+Noxgoes@users.noreply.github.com>
2025-07-25 04:18:04 +0530

    Delete envConfig.js

--- envConfig.js
+++
@@ -1,5 +0,0 @@
-import {loadEnvConfig} from '@next/env';
-const projectDir = process.cwd();
-loadEnvConfig(projectDir);
-// This will load environment variables from .env.local, .env.development, and .env.production files based on the current environment.
-//to allow read env variables
